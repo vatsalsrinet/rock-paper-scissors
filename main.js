5aa0030fbf515370a301d1eb68063e306648bd12
Vatsal Srinet <vatsalsrinetcodee@gmail.com>
2026-04-14 01:30:26 -0400

    added the play round function with its correct logic

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
 function getComputerChoice() {
     x = Math.random() * 3;
-    if (x < 1){
-        return "Rock";
+    if (x < 1) {
+        return "rock";
     }
-    if(x < 2){
-        return "Scissors";
+    if (x < 2) {
+        return "scissors";
     }
-    return "Paper";
+    return "paper";
 }
-console.log(getComputerChoice());
+function getHumanChoice() {
+    return prompt("Rock, Paper, or Scissors?");
+}
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (computerChoice == humanChoice) {
+        console.log(`Tie! Both you and the  computer picked ${computerChoice}`);
+    }
+    else if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+            console.log(`You lost, Paper beats Rock!`);
+            computerScore++;
+        }
+        else {
+            console.log(`You won, Rock beats Scissors!`);
+            humanScore++;
+        }
+    }
+    else if (humanChoice == "paper") {
+        if (computerChoice == "scissors") {
+            console.log(`You lost, Scissors beats Paper!`);
+            computerScore++;
+        }
+        else {
+            console.log(`You won, Paper beats Rock!`);
+            humanScore++;
+        }
+    }
+    else if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log(`You lost, Rock beats Scissors!`);
+            computerScore++;
+        }
+        else {
+            console.log(`You won, Scissors beats Paper!`);
+            humanScore++;
+        }
+    }
+}
